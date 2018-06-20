3b455dc60a91d84c7c261636329aca42d1f30022
wesleyjiang <jiang.yuto@husky.neu.edu>
2018-06-20 11:24:52 -0400

    allow access origin

--- server.js
+++ server.js
@@ -25,6 +25,17 @@ app.use(function(req, res, next) {
     next();
 });
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin",
+        "https://webdev-angular-hw.herokuapp.com/home");
+    res.header("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
+
 var session = require('express-session');
 app.use(session({
     resave: true,
