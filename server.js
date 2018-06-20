e58e1a685b737827bcf57764cb399e6b24c42a47
wesleyjiang <jiang.yuto@husky.neu.edu>
2018-06-20 16:50:52 -0400

    change port and access origin

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "https://webdev-angular-hw.herokuapp.com");
-    res.header("Access-Control-Allow-Origin", "https://localhost:4200");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Credentials", "true");
